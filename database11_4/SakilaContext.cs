d55347b2607808d49628d73c42b25e109fee9695
seamo88 <bstockgame.88@gmail.com>
2020-04-12 14:52:14 -0500

    Add project files.

---
+++ database11_4/SakilaContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database11_4
+{
+    class SakilaContext : DbContext
+    {
+        public DbSet<Film> Film { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;")
+;
+        }
+    }
+}
+
+
+
